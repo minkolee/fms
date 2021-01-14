de213b7cb608459e04cb1ef6cb30743dcc823da0
minkolee@gmail.com <jenny0217!@>
2021-01-14 16:36:11 +0800

    启动了项目APP，略微修改首页

--- fms/settings.py
+++ fms/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage.apps.HomepageConfig'
+    'homepage.apps.HomepageConfig',
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
