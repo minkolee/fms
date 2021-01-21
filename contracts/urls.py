646dad0530e9abaea6dd6b9071a8e1edd393f04f
minkolee <minkolee@gmail.com>
2021-01-21 23:31:31 +0800

    添加初始化项目的功能菜单
    做好编写合同的准备

--- contracts/urls.py
+++ contracts/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 app_name = 'contracts'
 
-urlpatterns = [
+# URL设计想法如下
+# contract/list/<int:project_id>/ 列出一个项目下所有合同
+# contract/add/<int:project_id>/ 为当前项目添加合同
+# contract/edit/<int:project_id>/<int:contract_id>/ 修改某个项目的合同
+# contract/delete/<int:contract_id>/ 删除合同（非管理员禁止）
+#
 
+urlpatterns = [
+    path('initialize/', views.initialize_contract_type, name='contract_type_initial'),
 ]
 
--- contracts/views.py
+++ contracts/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from projects.models import Project
-
+from django.contrib import messages
 from .models import ContractType
 
 
 # 初始化合同类型的函数，找个链接执行一下即可。考虑将最后一个菜单设置为系统维护。
-def initialize_contract_type(request, project_id):
+def initialize_contract_type(request):
     types = ContractType.objects.all()
 
     names = {1: '收入合同',
              2: '成本合同',
-             3: '其他合同'}
+             3: '其他合同',
+             4: '非合同'}
 
     for each_type in types:
         each_type.delete()
 
-    for i in range(1, 4):
+    for i in range(1, 5):
         con = ContractType()
         con.id = i
         con.name = names[i]
         con.save()
 
-    return render(request, 'projects/project_list.html')
+    messages.success(request, '成功初始化合同类型')
+
+    return render(request, 'homepage/dashboard.html')
