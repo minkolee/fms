0473b19c72d6bf5efce93f446acb901caf28a9ae
minkolee@gmail.com <jenny0217!@>
2021-01-22 10:33:15 +0800

    回家写，在工作就安心工作

--- contracts/models.py
+++ contracts/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class ContractType(models.Model):
     id = models.IntegerField(choices=((1, '收入合同'), (2, '成本合同'), (3, '其他合同'), (4, '非合同')), primary_key=True)
     name = models.CharField(max_length=4, verbose_name="类型名称")
+
+
+class Contract(models.Model):
+    pass
